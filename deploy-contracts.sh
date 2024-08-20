0c52341d759370e3ba653f7060568a3f0cb9ec84
Nenad <xinef.it@gmail.com>
2024-08-20 19:21:00 +0200

    Enable redeploying contracts

--- deploy-contracts.sh
+++ deploy-contracts.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+deploy_command="npx hardhat ignition deploy ignition/modules/HHSN.ts"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --reset)
+        deploy_command+=" --reset"
+        export HARDHAT_IGNITION_CONFIRM_RESET=false
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
 networks=(sepolia polygonAmoy)
 dir_array=(./typechain-types ./artifacts ./cache)
 
@@ -9,7 +25,7 @@ export HARDHAT_IGNITION_CONFIRM_DEPLOYMENT=false
 
 for network in "${networks[@]}"; do
     echo "Deploying to $network..."
-    npx hardhat ignition deploy ignition/modules/HHSN.ts --network "$network" --parameters "ignition/parameters/$network.json"
+    eval "$deploy_command --network $network --parameters ignition/parameters/$network.json"
 done
 
 set +e
