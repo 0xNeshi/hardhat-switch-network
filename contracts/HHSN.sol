982f2b4e8233ead960d829f623cc3b067ba299fa
Nenad <xinef.it@gmail.com>
2024-08-20 11:35:34 +0200

    Remove txs test from CI + update contract to allow only owner to call

--- contracts/HHSN.sol
+++ contracts/HHSN.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.0;
 
 contract HHSN {
     string public message;
+    address public owner;
 
     constructor(string memory _message) {
         message = _message;
+        owner = msg.sender;
     }
 
     function update(string memory _message) public {
+        require(msg.sender == owner, "Only owner can update the message");
         message = _message;
     }
 }
--- deploy-contracts.sh
+++ deploy-contracts.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+set -e
+
 networks=(sepolia polygonAmoy)
+dir_array=(./typechain-types ./artifacts ./cache)
+
 export HARDHAT_IGNITION_CONFIRM_DEPLOYMENT=false
+
 for network in "${networks[@]}"; do
     echo "Deploying to $network..."
-    npx hardhat ignition deploy ignition/modules/HHSN.ts --network "$network" --parameters "ignition/parameters/$network.json" --verify
+    npx hardhat ignition deploy ignition/modules/HHSN.ts --network "$network" --parameters "ignition/parameters/$network.json"
+done
+
+set +e
+
+for chain_path in ./ignition/deployments/*; do
+    chain=$(basename "$chain_path")
+    npx hardhat ignition verify "$chain" --include-unrelated-contracts
 done
 
 for proj in ./samples/*; do
-    cp -r ./typechain-types "$proj"
-    rm -rf ./typechain-types
-    cp -r ./artifacts "$proj"
-    rm -rf ./artifacts
-    cp -r ./cache "$proj"
-    rm -rf ./cache
+    for dir in "$dir_array"; do
+        cp -r "$dir" "$proj"
+        rm -rf "$dir"
+    done
 done
