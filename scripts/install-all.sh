71c8bd6a7c407055a87b6b0ac42ecfc706e4bfa1
0xNeshi <xinef.it@gmail.com>
2025-12-06 07:09:53 +0100

    skip web3 on latest

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -12,7 +12,7 @@ export HARDHAT_IGNITION_CONFIRM_RESET=false
 while [[ "$#" -gt 0 ]]; do
     case $1 in
     --latest)
-        command="rm -f package-lock.json && npm install --legacy-peer-deps"
+        command="rm -f package-lock.json && npm uninstall @nomicfoundation/hardhat-web3-v4 && npm install"
         ;;
     *)
         echo "Unknown option: $1"
@@ -25,6 +25,12 @@ done
 eval "$command"
 
 for proj in ./examples/*; do
+    proj_name=$(basename "$proj")
+    if [[ "$proj_name" == "web3-v4-project" ]]; then
+        echo "Skipping $proj_name"
+        continue
+    fi
+ 
     cd "$proj"
     eval "$command"
     cd ../..
--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -4,7 +4,15 @@ echo "Running query tests in all projects..."
 
 npm test
 
+skip_project="$1"
+
 for proj in ./examples/*; do
+    proj_name=$(basename "$proj")
+    if [[ "$proj_name" == "$skip_project" ]]; then
+        echo "Skipping $proj_name"
+        continue
+    fi
+ 
     cd "$proj"
     npm test
     cd ../..
